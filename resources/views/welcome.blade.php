cf80f37b32e45b47122b15737198d8abb61b4839
vmthanki <vivekthanki9082@gmail.com>
2019-04-18 17:47:07 +0530

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Larav
+                    Lara
                 </div>
 
                 <div class="links">
