a6ebc56dc992469dc0ad571f0a622d20f4a8a71a
vmthanki <vivekthanki9082@gmail.com>
2019-04-18 17:56:33 +0530

    welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Lara
+                   vivek thanki
                 </div>
 
                 <div class="links">
