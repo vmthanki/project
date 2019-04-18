52a82df8fe7265aa3a5864fcefe9240a111c98f7
vmthanki <vivekthanki9082@gmail.com>
2019-04-18 19:48:16 +0530

    welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   welcome
+                   welcome to laravel
                 </div>
 
                 <div class="links">
